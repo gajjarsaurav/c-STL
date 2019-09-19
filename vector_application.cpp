678cd1e0751a29fb6a2a1c3abf2fb4abfe9d9c82
saurabh gajjar <saurabhgajjar02@gmail.com>
2019-09-19 19:12:28 +0530

    Add files via upload

---
+++ vector_application.cpp
@@ -0,0 +1,15 @@
+#include <iostream> 
+#include <vector>
+#include <curses.h>
+using namespace std;
+int main(){
+//  addition in vector
+int sum = 0;
+    vector<int> v1{1, 2, 3, 4, 5};
+    while(v1.size() > 0){
+        sum += v1.back();
+        v1.pop_back();
+    }
+    cout<<sum;
+    return 0;
+}
